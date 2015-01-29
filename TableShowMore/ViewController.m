17a40af6336c4378234f3f942eac4079f0d7a36f
duskyHu <laohu@huzejundeMacBook-Pro.local>
2015-01-29 21:13:39 +0800

    tableDemo
    点击加载更多的demo

--- TableShowMore/ViewController.m
+++ TableShowMore/ViewController.m
@@ -8,20 +8,83 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>{
+    int index;
+}
 
 @end
 
 @implementation ViewController
 
+-(void)loadView{
+    [super loadView];
+    index = -1;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 44;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == index) {
+        return 44;
+    }
+    return 0;
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 44)];
+    UIButton *btn = [[UIButton alloc]initWithFrame:CGRectMake(200, 0, 120, 44)];
+    btn.tag = section;
+    btn.backgroundColor = [UIColor grayColor];
+    [btn setTitle:[NSString stringWithFormat:@"第%d组信息",(int)section+1] forState:0];
+    [btn addTarget:self action:@selector(changeinfo:) forControlEvents:UIControlEventTouchDown];
+    [view addSubview:btn];
+    return view;
+}
+
+-(void)changeinfo:(id)sender{
+    if (index < 0) {
+        index = (int)[sender tag];
+    }else if(index == [sender tag]){
+        index = -1;
+    }else{
+        index = (int)[sender tag];
+    }
+//    index = (int)[sender tag];
+    [_tableView reloadData];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 12;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section == index) {
+        return 5;
+    }
+    return 0;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell = [[UITableViewCell alloc]init];
+    cell.textLabel.text = [NSString stringWithFormat:@"第%d组第%d行",(int)indexPath.section+1,(int)indexPath.row + 1];
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+
 }
 
 @end
